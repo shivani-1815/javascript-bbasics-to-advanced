f0a91cc2564abad015468448e582f43303071943
shivani-1815 <shivanimaurya15vns@gmail.com>
2025-08-13 19:47:53 +0000

    datatype

---
+++ 02_dataType.js
@@ -0,0 +1,17 @@
+"use strict";//treat all js code as newer version
+// alert("hello"); // we are using nod ejs not browser
+
+// code readability should be high
+
+
+//number => 2 to power 53
+//bigint 
+//string=> ""
+//boolean =>true/false
+// null =>standalone value
+//undefined =>variable declare but no tassign the value
+//symbol =>uniquness  ke liye jb baat ho to 
+//object
+console.log(typeof null); // output object
+console.log(typeof undefined); output undefined
+ 
---
+++ basics/01_variable.js
@@ -0,0 +1,7 @@
+accountId = 7890
+accountEmail ="hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword ,accountCity])
+/*prefer not to use var because of issue in block scope and functional scope*/
